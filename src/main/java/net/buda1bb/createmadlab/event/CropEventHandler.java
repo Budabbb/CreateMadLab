68bd3d3f3b868a1d80fbb83b5cb21178abe898e3
Buda <lestibuda@gmail.com>
2025-12-31 11:39:01 +0100

    2.3.1 release HOTFIX: Now works on servers, this time for real

--- src/main/java/net/buda1bb/createmadlab/event/CropEventHandler.java
+++ src/main/java/net/buda1bb/createmadlab/event/CropEventHandler.java
@@ -1,9 +1,8 @@
 package net.buda1bb.createmadlab.event;
 
 import net.buda1bb.createmadlab.CreateMadLab;
-import net.buda1bb.createmadlab.block.ModBlocks;
-import net.buda1bb.createmadlab.block.ErgotInfestedWheatBlock;
 import net.minecraft.core.BlockPos;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.CropBlock;
@@ -11,6 +10,7 @@ import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.level.BlockEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.registries.ForgeRegistries;
 
 @Mod.EventBusSubscriber(modid = CreateMadLab.MOD_ID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class CropEventHandler {
@@ -21,13 +21,28 @@ public class CropEventHandler {
 
         BlockPos pos = event.getPos();
         BlockState state = serverLevel.getBlockState(pos);
+        ResourceLocation ergotWheatId = new ResourceLocation("createmadlab", "ergot_infested_wheat");
+        var ergotWheatBlock = ForgeRegistries.BLOCKS.getValue(ergotWheatId);
 
-        if (state.getBlock() == ModBlocks.ERGOT_INFESTED_WHEAT.get() &&
-                state.hasProperty(ErgotInfestedWheatBlock.AGE) &&
-                state.getValue(ErgotInfestedWheatBlock.AGE) == 0) {
+        if (ergotWheatBlock == null) return;
 
-            serverLevel.setBlock(pos, Blocks.WHEAT.defaultBlockState()
-                    .setValue(CropBlock.AGE, 0), 3);
+        if (state.getBlock() == ergotWheatBlock) {
+            try {
+                var agePropertyField = ergotWheatBlock.getClass().getDeclaredField("AGE");
+                if (agePropertyField != null) {
+                    var ageProperty = (net.minecraft.world.level.block.state.properties.IntegerProperty) agePropertyField.get(null);
+
+                    if (state.hasProperty(ageProperty) && state.getValue(ageProperty) == 0) {
+                        serverLevel.setBlock(pos, Blocks.WHEAT.defaultBlockState()
+                                .setValue(CropBlock.AGE, 0), 3);
+                    }
+                }
+            } catch (Exception e) {
+                if (state.getBlock() == ergotWheatBlock) {
+                    serverLevel.setBlock(pos, Blocks.WHEAT.defaultBlockState()
+                            .setValue(CropBlock.AGE, 0), 3);
+                }
+            }
         }
     }
 }
